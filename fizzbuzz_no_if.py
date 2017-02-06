95bc7357a4fc416261f68274f05c65970846ffa4
Dennis Gomer <dgomer@edjanalytics.com>
2017-02-06 10:08:31 -0500

    Now with comments!

--- fizzbuzz_no_if.py
+++ fizzbuzz_no_if.py
@@ -1,8 +1,16 @@
 def fizzbuzz(run_to):
-    fizz_dict = {i:"Fizz" for i in range(3, run_to+1, 3)}
-    for j in range(5, run_to+1, 5):
+    ''' FizzBuzz, in constant memory space, with no if statements.
+
+    Arguments:
+        run_to: The integer value (>0) to generate FizzBuzzes up to. 
+    '''
+    fizz_num = 3
+    buzz_num = 5
+    period = fizz_num * buzz_num
+    fizz_dict = {i:"Fizz" for i in range(0, period+1, 3)}
+    for j in range(0, period+1, 5):
         fizz_dict[j] = fizz_dict.get(j, '') + "Buzz"
     for k in range(1, run_to+1):
-        print(fizz_dict.get(k, k))
+        print(fizz_dict.get(k%period, k))
 
 fizzbuzz(100)
