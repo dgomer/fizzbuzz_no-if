7a193cb60ec889daeb04e928329389d78651f0e7
Dennis Gomer <dgomer@edjanalytics.com>
2017-02-06 10:19:53 -0500

    Fix for unnecessary range generation

--- fizzbuzz_no_if.py
+++ fizzbuzz_no_if.py
@@ -7,8 +7,8 @@ def fizzbuzz(run_to):
     fizz_num = 3
     buzz_num = 5
     period = fizz_num * buzz_num
-    fizz_dict = {i:"Fizz" for i in range(0, period+1, 3)}
-    for j in range(0, period+1, 5):
+    fizz_dict = {i:"Fizz" for i in range(0, period, 3)}
+    for j in range(0, period, 5):
         fizz_dict[j] = fizz_dict.get(j, '') + "Buzz"
     for k in range(1, run_to+1):
         print(fizz_dict.get(k%period, k))
